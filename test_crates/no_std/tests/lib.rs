6bb2a28de98f171c3d073cfa170ff18e00599164
Jun Luo <4catcode@gmail.com>
2024-09-11 10:40:33 +0800

    WIP

---
+++ test_crates/no_std/tests/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+// See https://crates.io/crates/defmt-test/0.3.0 for more documentation (e.g. about the 'state'
+// feature)
+#[defmt_test::tests]
+mod tests {
+    use defmt::assert;
+
+    #[test]
+    fn it_works() {
+        assert!(true)
+    }
+}
